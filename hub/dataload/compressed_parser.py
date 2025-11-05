816d8e518b0161d486e70c8110829519a588a7da
shuchenliu <shuchenliu2014@gmail.com>
2025-11-04 20:01:27 -0800

    update load_from_tar method to select newest tar file by default unless directed by the plugin manifest

--- hub/dataload/compressed_parser.py
+++ hub/dataload/compressed_parser.py
@@ -1,8 +1,11 @@
+import glob
+import os
 import pathlib
-from typing import Union, Literal
+from typing import Union, Literal, Optional
 
 import jsonlines
 from biothings.utils.common import open_anyfile
+from biothings.utils.info import logger
 
 from hub.dataload.data_parsers import EDGE_BUFFER_SIZE
 from hub.dataload.utils.file import buffered_yield
@@ -30,9 +33,30 @@ def read_compressed(
                 yield doc
 
 
-def load_from_tar(data_folder: Union[str, pathlib.Path], file_name: str, entity: Literal['edges', 'nodes'], gen_id=True):
-    """ Stream edge data from given JSONL file """
+def load_from_tar(data_folder: Union[str, pathlib.Path], entity: Literal['edges', 'nodes'], file_name:Optional[str]=None, gen_id=True):
+    """ Stream data from given JSONL file """
     data_folder = pathlib.Path(data_folder).resolve().absolute()
-    tar_file = data_folder.joinpath(file_name)
+
+    if file_name is None:
+        files = [
+            f for f in glob.glob(os.path.join(data_folder, "*"))
+            if not os.path.basename(f).startswith("._")
+        ]
+
+        if not files:
+            raise FileNotFoundError("No valid tar files found.")
+
+        # picking the newest files
+        tar_file = max(files, key=os.path.getmtime)
+
+        if len(files) > 1:
+            logger.warning(f"Warning: multiple files found, picking newest: {tar_file}")
+    else:
+        # use file_name if provided
+        tar_file = data_folder.joinpath(file_name)
+
+    if not tar_file.exists():
+        raise FileNotFoundError(f"File {tar_file} not found.")
+
     target_file = f"{entity}.jsonl"
     yield from read_compressed(tar_file, target_file, gen_id, expect_id=True)
