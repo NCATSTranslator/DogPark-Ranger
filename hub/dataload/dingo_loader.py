4caddcd76baad97be2236c3f6743f4d5efdc100c
shuchenliu <shuchenliu2014@gmail.com>
2025-11-06 15:38:54 -0800

    add loader for DINGO datasets

--- hub/dataload/dingo_loader.py
+++ hub/dataload/dingo_loader.py
@@ -1,17 +1,26 @@
+from functools import partial
+
 from hub.dataload.compressed_parser import load_from_tar
-from hub.dataload.utils.pipeline import apply_processors, id_processor
+from hub.dataload.utils.pipeline import apply_processors
+from hub.dataload.utils.process_category import process_category_list
+from hub.dataload.utils.process_predicate import process_predicate
+from hub.dataload.utils.process_qualifiers import process_qualifiers
+from hub.dataload.utils.process_sources import process_sources
 
 
-def dingo_node_processor(node):
+def node_processor(node):
     processors = [
-        id_processor
+        process_category_list
     ]
     return apply_processors(processors, node)
 
 
-def dingo_edge_processor(edge):
+def edge_processor(predicate_cache: dict, edge):
     processors = [
-        id_processor
+        process_qualifiers,
+        process_sources,
+        # we need cache to do faster ancestor look up
+        partial(process_predicate, predicate_cache=predicate_cache)
     ]
     return apply_processors(processors, edge)
 
@@ -22,6 +31,10 @@ def parser(*args, **kwargs):
     if entity is None:
         raise ValueError("No entity specified")
 
-    processor_pipeline = dingo_node_processor if entity == "nodes" else dingo_edge_processor
+    processor_pipeline = (
+        node_processor
+        if entity == "nodes"
+        else partial(edge_processor, {}) # initialize predicate look-up cache
+    )
 
     yield from map(processor_pipeline, load_from_tar(*args, **kwargs))
