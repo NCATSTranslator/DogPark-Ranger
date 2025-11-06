9e31f20e4f927ce4615a45634d642df23448c95a
shuchenliu <shuchenliu2014@gmail.com>
2025-11-05 21:20:45 -0800

    update predicate processor

--- hub/dataload/utils/process_predicate.py
+++ hub/dataload/utils/process_predicate.py
@@ -1,12 +1,14 @@
 from hub.dataload.utils.postprocessing import get_ancestors, remove_biolink_prefix
 
+ORIGIN_FIELD_NAME = "predicate"
+TARGET_FIELD_NAME = "predicate_ancestors"
 
 def process_predicate(edge, predicate_cache: dict):
-    predicate = edge.get("predicate")
+    predicate = edge.get(ORIGIN_FIELD_NAME)
 
     if predicate:
         ancestors = get_ancestors(predicate, predicate_cache)
         if ancestors:
-            edge["all_predicates"] = ancestors
+            edge[TARGET_FIELD_NAME] = ancestors
 
-        edge["predicate"] = remove_biolink_prefix(predicate)
+        edge[ORIGIN_FIELD_NAME] = remove_biolink_prefix(predicate)
