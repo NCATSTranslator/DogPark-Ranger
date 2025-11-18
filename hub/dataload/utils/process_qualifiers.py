9cbd2e838dd97cacf414704f3e47e6461f17f0ff
shuchenliu <shuchenliu2014@gmail.com>
2025-11-17 21:36:07 -0800

    add biolink prefix removal to qualifier processor

--- hub/dataload/utils/process_qualifiers.py
+++ hub/dataload/utils/process_qualifiers.py
@@ -1,4 +1,4 @@
-from hub.dataload.utils.postprocessing import biolink
+from hub.dataload.utils.postprocessing import biolink, remove_biolink_prefix
 
 TARGET_FIELD = "qualifiers"
 
@@ -13,9 +13,12 @@ def process_qualifiers(edge):
 
     for field in list(edge.keys()):
         if biolink.is_qualifier(field):
+            if type(edge[field]) is not str:
+                raise TypeError("entry of an qualifier value must be a string")
+
             qualifier_fields.append({
-                QUAL_TYPE_NAME: field,
-                QUAL_VALUE: edge[field]
+                QUAL_TYPE_NAME: remove_biolink_prefix(field),
+                QUAL_VALUE: remove_biolink_prefix(edge[field])
             })
 
             edge.pop(field)
