98a9ac7a2073b0b89cbfa43d33c6924a8f0fc1d5
shuchenliu <shuchenliu2014@gmail.com>
2025-12-04 12:52:31 -0800

    add read from uncompressed pipeline

--- hub/dataload/kg_loader.py
+++ hub/dataload/kg_loader.py
@@ -1,6 +1,7 @@
 from functools import partial
 
 from hub.dataload.compressed_parser import load_from_tar
+from hub.dataload.data_parsers import loader
 from hub.dataload.utils.pipeline import apply_processors
 from hub.dataload.utils.process_category import process_category_list
 from hub.dataload.utils.process_predicate import process_predicate
@@ -24,6 +25,27 @@ def edge_processor(predicate_cache: dict, edge):
         partial(process_predicate, predicate_cache=predicate_cache)
     ]
     return apply_processors(processors, edge)
+
+def parser_uncompressed(*args, **kwargs):
+    entity = kwargs.get('entity')
+
+    if entity is None:
+        raise ValueError("No entity specified")
+
+    processor_pipeline = (
+        node_processor
+        if entity == "nodes"
+        else partial(edge_processor, {})  # initialize predicate look-up cache
+    )
+
+    # disable sequence generation by default
+    if entity == "nodes" and kwargs.get('gen_seq', None) is None:
+        kwargs['gen_seq'] = False
+
+    kwargs['gen_id'] = True
+    kwargs['expect_id'] = True
+
+    yield from map(processor_pipeline, loader(*args, **kwargs))
 
 
 def parser(*args, **kwargs):
