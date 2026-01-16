e353adb7ea990a674f7a84bdc600e4d3b8deafb2
shuchenliu <shuchenliu2014@gmail.com>
2026-01-16 15:11:21 -0800

    add parser for ubergraph info processing

--- hub/dataload/kg_loader.py
+++ hub/dataload/kg_loader.py
@@ -1,4 +1,11 @@
+import base64
+import math
+import zlib
+from collections import defaultdict
 from functools import partial
+from typing import Iterable
+
+import msgpack
 
 from hub.dataload.compressed_parser import load_from_tar
 from hub.dataload.utils.pipeline import apply_processors
@@ -45,3 +52,98 @@ def parser(*args, **kwargs):
         kwargs['gen_seq'] = False
 
     yield from map(processor_pipeline, load_from_tar(*args, **kwargs))
+
+
+
+
+def get_adj_list(edge_iterator: Iterable) -> dict[str, list[str]]:
+    adj_set = defaultdict(set[str])
+
+    for edge in edge_iterator:
+        sub = edge['subject']
+        obj = edge['object']
+        adj_set[sub].add(obj)
+
+    adj_list: dict[str, list[str]] = {
+        k: list(v) for k, v in adj_set.items()
+    }
+
+    return adj_list
+
+def encapsule(payload: list | dict) -> str:
+
+    # todo test pickle
+    payload_b = msgpack.packb(payload, use_bin_type=True)
+
+    # todo zst
+    compressed_b = zlib.compress(payload_b, level=6)
+    base64_payload = base64.b64encode(compressed_b).decode("ascii")
+
+    return base64_payload
+
+
+def split_n_chunks(b64: str, n: int, key: str, offset = 0) -> list:
+    if n <= 0:
+        raise ValueError("n must be > 0")
+
+    length = len(b64)
+    chunk_size = math.ceil(length / n)
+
+    return [
+        {
+            "_id": str(offset + i),
+            "key": key,
+            "chunk_index": i,
+            "value": b64[i : i + chunk_size]
+        } for i in range(0, length, chunk_size)
+    ]
+
+
+def flat_parser(*args, **kwargs):
+    result_key = kwargs.pop('result_key')
+    adj_list_key = kwargs.pop('adj_list_key')
+
+    assert isinstance(result_key, str)
+    assert isinstance(adj_list_key, str)
+    assert result_key != adj_list_key
+
+    # disable extra payload
+    kwargs['gen_seq'] = False
+    kwargs['gen_id'] = False
+
+    edge_kwargs = {
+        **kwargs,
+        "entity": "edges"
+    }
+
+    node_kwargs = {
+        **kwargs,
+        "entity": "nodes"
+    }
+
+    edge_iterator = parser(*args, **edge_kwargs)
+    adj_list = get_adj_list(edge_iterator)
+    nodes = list(parser(*args, **node_kwargs))
+
+    # todo encapsule one unified object
+    nodes_b = encapsule(nodes)
+    adj_list_b = encapsule(adj_list)
+
+
+    node_chunks = 10 ** 3
+    adj_list_chunks = 10 ** 3
+
+    payload = []
+
+    node_chunks = split_n_chunks(nodes_b, node_chunks, result_key)
+    payload.extend(node_chunks)
+    payload.extend(split_n_chunks(adj_list_b, adj_list_chunks, adj_list_key, len(node_chunks)))
+
+    return payload
+
+
+
+
+
+
+
